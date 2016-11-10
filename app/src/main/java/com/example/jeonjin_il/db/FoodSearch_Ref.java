401f4f95011ead2fc233f1ce058ae20f28a31f68
jinil <il6339@naver.com>
2016-11-10 16:53:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jeonjin_il/db/FoodSearch_Ref.java
@@ -0,0 +1,17 @@
+package com.example.jeonjin_il.db;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jeonjin-il on 2016. 11. 10..
+ */
+public class FoodSearch_Ref extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_ref);
+    }
+}
+
